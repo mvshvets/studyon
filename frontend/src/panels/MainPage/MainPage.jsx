2213b35bda275c8dc1c64e33f320e8ea98a83f58
Максим Швец <shvetsmv@it2g.ru>
2020-05-03 15:49:45 +0300

    Фикс адаптива

--- frontend/src/panels/MainPage/MainPage.jsx
+++ frontend/src/panels/MainPage/MainPage.jsx
@@ -1,14 +1,14 @@
 import React, {useState} from 'react'
 import {Gallery} from '@vkontakte/vkui'
 import {Useful, Profile, Rating, Store, Zoom, Prospects} from './components'
+import './MainPage.css';
 
 export const MainPage = () => {
     const [slideIndex, setSlideIndex] = useState(0)
 
     return (
         <Gallery
-            slideWidth={'100%'}
-            style={{height: '100%'}}
+            style={{height: '100vh'}}
             bullets="light"
             slideIndex={slideIndex}
             onChange={slideIndex => {
