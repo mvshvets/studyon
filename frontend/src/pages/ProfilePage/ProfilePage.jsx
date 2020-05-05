cf8b3bffd5053d0abf6ee39f2e361f76162f92b3
Максим Швец <shvetsmv@it2g.ru>
2020-05-05 04:00:47 +0300

    Сделал запросы для степика

--- frontend/src/pages/ProfilePage/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage/ProfilePage.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, {useContext, useEffect} from 'react'
 import {Header, Footer} from '../../core/components'
 import {student} from '../../shared/img'
 import style from './ProfilePage.module.sass'
 import {Progress, PiggyBank, Strides, ConnectOtherApp} from './components'
+import bridge from '@vkontakte/vk-bridge'
+import axios from 'axios-jsonp-pro'
+import {UserContext} from '../../core/context/User'
 
 export const ProfilePage = React.memo(props => {
+    const {setUserData} = useContext(UserContext)
+
+    const fetchData = async () => {
+
+        const userVK = await bridge.send('VKWebAppGetUserInfo');
+
+        let urlUsers = "http://3.19.63.58:8080/user/"+ userVK.id;
+        let userData = await axios.jsonp(urlUsers)
+        console.log(userData);
+        setUserData(userData)
+
+        const token = await bridge.send("VKWebAppGetAuthToken", { "app_id": 7432322, "scope": "friends, video, groups" });
+        console.log(token.access_token);
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [fetchData])
+
     return (
         <>
             <Header title={'ПРОФИЛЬ'} background={'purple'} icon={student} isSettings/>
--- frontend/src/pages/ProfilePage/components/ConnectOtherApp/ConnectOtherApp.jsx
+++ frontend/src/pages/ProfilePage/components/ConnectOtherApp/ConnectOtherApp.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, {useCallback, useContext} from 'react'
 import {default as stepik} from './stepik.png'
 import style from './ConnectOtherApp.module.sass'
 import {Button, ButtonIcon, Text14} from '../../../../shared/components'
 import {hint_icon} from '../PiggyBank/img'
+import bridge from '@vkontakte/vk-bridge'
+import axios from 'axios-jsonp-pro'
+import {UserContext} from '../../../../core/context/User'
 
 export const ConnectOtherApp = React.memo(() => {
+    const {userData, setUserData} = useContext(UserContext)
 
+    const authStepik = useCallback(async () => {
+
+        const user = await bridge.send('VKWebAppGetUserInfo')
+
+        let urlUsers = 'http://3.19.63.58:8080/user/' + user.id
+        let friends = await axios.post(urlUsers, {accountList: [
+                {
+                    "accountService": "STEPIK",
+                    "authorizationCode": "KBq1AGuEgpBoqK2q91MbUYovXgKVmk"
+                }
+            ]})
+        let userData = friends.status === 201 || friends.status === 201 ? await axios.jsonp(urlUsers) : null
+        setUserData(userData)
+
+    }, [])
     return (
         <div className={style.connectOtherApp}>
             <div>
@@ -16,7 +35,11 @@ export const ConnectOtherApp = React.memo(() => {
                         <Text14 text={'Study hard. Teach harder.'} className={style.text2}/>
                     </div>
                 </div>
-                {false ? <Text14 text={'Аккаунт на Stepik привязан'}/> : <Button text={<Text14 text={'Привязать аккаунт на Stepik'}/>} className={style.button}/>}
+                {userData.accounts.stepik ?
+                    <Text14 text={'Аккаунт на Stepik привязан'}/>
+                    :
+                    <Button text={<Text14 text={'Привязать аккаунт на Stepik'}/>} className={style.button} onClick={authStepik}/>
+                }
             </div>
             <img src={stepik} alt={'STEPIK'} className={style.logo}/>
         </div>
